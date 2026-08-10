e1102d1c82e82a52091a89cfd70693f7bceb7ee8
Mdkhubaib861 <mdkhobaib861@gmail.com>
2026-08-10 17:55:16 +0530

    update

--- string_functions_practice.sql
+++ string_functions_practice.sql
@@ -2,6 +2,22 @@ use ecommercedb;
 
 -- SELECT * FROM products;
 -- SELECT * FROM categories;
+/*
 SELECT c.categoryname, p.productname, CONCAT(LEFT(c.categoryname, 3), LEFT(p.productname, 3)) AS result 
 FROM categories c INNER JOIN products p 
 ON c.categoryID = p.categoryID ;
+
+SELECT categoryID, Lpad(categoryID,4,'0') as CAT_NAME
+FROM categories;
+
+SELECT productName, LOCATE('o',productName) AS O_POS
+FROM PRODUCTS;
+
+SELECT CategoryName, REPLACE(CategoryName,"e","x") AS NEW_NAME
+FROM categories;
+
+SELECT productName, LOCATE('o',productName) AS O_POS
+FROM PRODUCTS
+WHERE LOCATE('o',productName) !=0;
+*/
+
